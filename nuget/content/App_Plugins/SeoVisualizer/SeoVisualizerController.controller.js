f284f3121b4d21ff7d22c3a3ff123feb978e30a2
Markus Johansson <markus@enkelmedia.se>
2014-05-08 01:41:08 +0200

    Added nuget package

---
+++ nuget/content/App_Plugins/SeoVisualizer/SeoVisualizerController.controller.js
@@ -0,0 +1,51 @@
+angular.module("umbraco")
+    .controller("EnkelMedia.SeoVisualizerController",
+    function ($scope) {
+        
+        $scope.title = $scope.model.value.title;
+        $scope.description = $scope.model.value.description;
+
+        $scope.$watch("title", function () {
+            $scope.UpdateModel();
+        });
+
+        $scope.$watch("description", function () {
+            $scope.UpdateModel();
+        });
+
+        $scope.UpdateModel = function () {
+            $scope.model.value = { title: $scope.title, description: $scope.description };
+        };
+
+
+        $scope.GetUrl = function() {
+            return $scope.ProtocolAndHost() + $scope.GetParentContent().urls[0];
+        };
+
+        /* ****** utility funcitons ******* */
+
+        // Returns the protocal and host for the current domain (ie. http://www.mainwebsite.com).
+        $scope.ProtocolAndHost = function() {
+            var http = location.protocol;
+            var slashes = http.concat("//");
+            return slashes.concat(window.location.hostname);
+        };
+
+        // Climbs the $scope.parent objects to find the parent that contains the "content"-property which
+        // has the url-property the we need to figure out the url of the current document.
+        $scope.GetParentContent = function() {
+            var currentScope = $scope.$parent;
+            
+            for (var i = 0; i < 150; i++) {
+                if (currentScope.content) {
+                    return currentScope.content;
+                }
+
+                currentScope = currentScope.$parent;
+            }
+
+            return null;
+
+        };
+
+    });
