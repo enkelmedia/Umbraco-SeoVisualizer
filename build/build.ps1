7e0be64c47e29f0f4e3342ce33883b42b6a3d0a3
Markus Johansson <markus@enkelmedia.se>
2023-07-15 22:51:23 +0200

    Build fix

--- build/build.ps1
+++ build/build.ps1
@@ -1,3 +1,5 @@
 dotnet pack ../SeoVisualizer.Core/SeoVisualizer.Core.csproj --output Artifacts --configuration Release
-dotnet pack ../SeoVisualizer/SeoVisualizer.csproj --output Artifacts --configuration Release
+
+dotnet build ../SeoVisualizer/SeoVisualizer.csproj --configuration Release
+dotnet pack ../SeoVisualizer/SeoVisualizer.csproj --output Artifacts --no-build --configuration Release
 
