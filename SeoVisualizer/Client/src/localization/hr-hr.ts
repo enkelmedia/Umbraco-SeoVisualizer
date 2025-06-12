2c98b911d19b28142bc6833918d1c3aaf8fb1c1b
alevak09 <73195407+alevak09@users.noreply.github.com>
2025-06-12 13:51:37 +0200

    Create hr-hr.ts (#41)
    Added croatian localization

---
+++ SeoVisualizer/Client/src/localization/hr-hr.ts
@@ -0,0 +1,11 @@
+import type { UmbLocalizationDictionary } from '@umbraco-cms/backoffice/localization-api';
+
+export default {
+  seoVisualizer: {
+    title_placeholder: 'Upišite naslov stranice',
+    description_placeholder: 'Upišite opis stranice',
+    max_length: 'Optimalna duljina je manja od %0% znakova.',
+    no_index: 'Izuzmi stranicu iz pretraživača',
+    exclude_title_suffix: 'Izuzmi zadani dodatak naslova',
+  }
+} as UmbLocalizationDictionary;
